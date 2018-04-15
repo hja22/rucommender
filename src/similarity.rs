60fb1d91c994870f30d7caadb40863e559d14097
Henry Addison <henryaddison@users.noreply.github.com>
2018-04-15 16:28:21 +0100

    add some unit tests to data extraction from csv
    refactor to make testing of row processing possible

--- src/similarity.rs
+++ src/similarity.rs
@@ -25,22 +25,10 @@ pub fn read_csv<R: io::Read>(activity_source: R) -> Result<(UserItemsMap, ItemUs
     let mut user_items = UserItemsMap::new();
     let mut item_users = ItemUsersMap::new();
 
-    let empty_items = HashSet::<ItemId>::new();
-    let empty_users = HashSet::<UserId>::new();
-
     for result in rdr.deserialize() {
         let record: Record = result?;
-        {
-            user_items.entry(record.user_id).or_insert(empty_items.clone());
-            let current_user_items = user_items.get_mut(&record.user_id).expect("item-score pairs");
-            current_user_items.insert(record.item_id);
-        }
-
-        {
-            item_users.entry(record.item_id).or_insert(empty_users.clone());
-            let current_item_users = item_users.get_mut(&record.item_id).expect("set of user ids");
-            current_item_users.insert(record.user_id);
-        }
+        add_item(&mut user_items, &record.user_id, &record.item_id);
+        add_user(&mut item_users, &record.item_id, &record.user_id);
     }
     Ok((user_items, item_users))
 }
@@ -67,4 +55,53 @@ pub fn compute_similarities(user_items: &UserItemsMap, item_users: &ItemUsersMap
         similarities.insert(*user_id, current_user_similarity);
     }
     Ok(similarities)
+}
+
+fn add_item(user_items_map: &mut UserItemsMap, user_id: &UserId, item_id: &ItemId) {
+    let empty_items = HashSet::<ItemId>::new();
+
+    user_items_map.entry(*user_id).or_insert(empty_items);
+    let current_user_items = user_items_map.get_mut(user_id).expect("cannot get items for given user id");
+    current_user_items.insert(*item_id);
+}
+
+fn add_user(item_users_map: &mut ItemUsersMap, item_id: &ItemId, user_id: &UserId) {
+    let empty_users = HashSet::<UserId>::new();
+
+    item_users_map.entry(*item_id).or_insert(empty_users);
+    let current_items_users = item_users_map.get_mut(item_id).expect("cannot get users for given items id");
+    current_items_users.insert(*user_id);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn add_item_to_new_user() {
+        let item_id = 101;
+        let user_id = 1;
+
+        let mut user_items = UserItemsMap::new();
+        add_item(&mut user_items, &user_id, &item_id);
+
+        let mut expected_user_items = UserItemsMap::new();
+        let expected_item_set: HashSet<ItemId> = [ item_id ].iter().cloned().collect();
+        expected_user_items.insert(user_id, expected_item_set);
+        assert_eq!(user_items, expected_user_items);
+    }
+
+    #[test]
+    fn add_user_to_new_item() {
+        let item_id = 101;
+        let user_id = 1;
+
+        let mut item_users = ItemUsersMap::new();
+        add_item(&mut item_users, &item_id, &user_id);
+
+        let mut expected_item_users = ItemUsersMap::new();
+        let expected_user_set: HashSet<UserId> = [ user_id ].iter().cloned().collect();
+        expected_item_users.insert(item_id, expected_user_set);
+        assert_eq!(item_users, expected_item_users);
+    }
 }
